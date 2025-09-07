49e1088417a49450eb2d173c4c17765213988ca2
renan2105 <orenan124@gmail.com>
2025-09-07 15:15:28 -0300

    Adicionando autenticação ao swagger

--- src/main/java/com/renan/config/OpenApiConfig.java
+++ src/main/java/com/renan/config/OpenApiConfig.java
@@ -1,18 +1,32 @@
 package com.renan.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("Mobiauto Backend API")
-                        .version("0.0.1")
-                        .description("Documentação OpenAPI gerada pelo springdoc-openapi"));
+                        .title("Mobiauto API")
+                        .description("Documentação da API Mobiauto com autenticação JWT")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
